512974f08dec707478aec571f21ce6fa74776b8c
Ubuntu <ubuntu@ip-172-31-90-165.ec2.internal>
2024-12-30 13:22:45 +0000

    Initial commit - stable version before delete feature

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,51 +1,59 @@
 class BooksController < ApplicationController
   before_action :set_book, only: %i[show edit update destroy]
-  before_action :load_books, only: %i[index show edit update destroy]
 
+  # GET /books
   def index
+    @books = Book.all
   end
 
+  # GET /books/1
   def show
   end
 
+  # GET /books/new
   def new
-    @book = {}
+    @book = Book.new
   end
 
-  def create
-    # Placeholder for adding books
-    redirect_to books_path
-  end
-
+  # GET /books/1/edit
   def edit
   end
 
-  def update
-    redirect_to books_path
+  # POST /books
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book, notice: 'Book was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
+  # PATCH/PUT /books/1
+  def update
+    if @book.update(book_params)
+      redirect_to @book, notice: 'Book was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /books/1
   def destroy
-    redirect_to books_path
+    @book.destroy
+    redirect_to books_url, notice: 'Book was successfully deleted.'
   end
 
   private
 
-  def load_books
-    @books = [
-      { id: 1, title: 'Neuromancer', author: 'William Gibson', description: 'The quintessential cyberpunk novel.' },
-      { id: 2, title: 'Snow Crash', author: 'Neal Stephenson', description: 'A futuristic world of hackers and virtual reality.' },
-      { id: 3, title: 'Altered Carbon', author: 'Richard K. Morgan', description: 'A gritty noir detective story in a cyberpunk future.' },
-      { id: 4, title: 'Do Androids Dream of Electric Sheep?', author: 'Philip K. Dick', description: 'A post-apocalyptic novel exploring humanity and AI.' },
-      { id: 5, title: 'The Peripheral', author: 'William Gibson', description: 'Alternate futures and virtual realities collide.' },
-      { id: 6, title: 'Count Zero', author: 'William Gibson', description: 'A thrilling sequel to Neuromancer.' },
-      { id: 7, title: 'The Diamond Age', author: 'Neal Stephenson', description: 'A futuristic tale of technology and education.' },
-      { id: 8, title: 'Hyperion', author: 'Dan Simmons', description: 'A far-future pilgrimage filled with dark secrets.' },
-      { id: 9, title: 'Dune', author: 'Frank Herbert', description: 'A sci-fi epic exploring politics and ecology.' },
-      { id: 10, title: 'Foundation', author: 'Isaac Asimov', description: 'The grand saga of the fall and rise of galactic civilization.' }
-    ]
+  # Use callbacks to share common setup or constraints between actions.
+  def set_book
+    @book = Book.find_by(id: params[:id])
+    redirect_to books_path, alert: 'Book not found.' if @book.nil?
   end
 
-  def set_book
-    @book = @books.find { |b| b[:id] == params[:id].to_i }
+  # Only allow a list of trusted parameters through.
+  def book_params
+    params.require(:book).permit(:title, :author, :description)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Book.create([
+  { title: "Neuromancer", author: "William Gibson", description: "The quintessential cyberpunk novel that started it all." },
+  { title: "Snow Crash", author: "Neal Stephenson", description: "A fast-paced, futuristic novel exploring virtual reality and linguistics." },
+  { title: "Altered Carbon", author: "Richard K. Morgan", description: "A hardboiled cyberpunk detective novel set in a world where consciousness can be transferred to new bodies." },
+  { title: "The Peripheral", author: "William Gibson", description: "A near-future thriller exploring technology and its impact on society." },
+  { title: "Count Zero", author: "William Gibson", description: "A sequel to Neuromancer, diving deeper into the world of cyberspace." },
+  { title: "Mona Lisa Overdrive", author: "William Gibson", description: "The third book in the Sprawl trilogy, tying up loose ends in the cyberpunk universe." },
+  { title: "Do Androids Dream of Electric Sheep?", author: "Philip K. Dick", description: "The novel that inspired Blade Runner, exploring AI, empathy, and humanity." },
+  { title: "The Diamond Age", author: "Neal Stephenson", description: "A tale of nanotechnology, education, and societal transformation." },
+  { title: "Ghost in the Shell", author: "Masamune Shirow", description: "A cyberpunk manga classic focusing on cyborgs and AI." },
+  { title: "When Gravity Fails", author: "George Alec Effinger", description: "A cyberpunk detective novel set in a futuristic Middle Eastern city." },
+  { title: "Schismatrix", author: "Bruce Sterling", description: "A posthuman epic about technological and biological modifications." },
+])
